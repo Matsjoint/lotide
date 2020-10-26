83d270de766b136eaa2377f4ca7ced9abae9ff23
Matsjoint <matsjoint@gmail.com>
2020-10-26 23:46:47 +0000

    hex code emojis &  custom assert function

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const good = String.fromCodePoint(0x2705);
+const noGood = String.fromCodePoint(0xd83d, 0xded1);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(good + " Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log(noGood + " Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
