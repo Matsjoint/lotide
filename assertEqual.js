e98f2386f229109bf4b16a05b88936d3286b499d
Matsjoint <matsjoint@gmail.com>
2020-10-26 23:53:07 +0000

    template literals edit

--- assertEqual.js
+++ assertEqual.js
@@ -2,11 +2,21 @@
 const good = String.fromCodePoint(0x2705);
 const noGood = String.fromCodePoint(0xd83d, 0xded1);
 
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(good + " Assertion Passed: " + actual + " === " + expected);
+//   } else {
+//     console.log(noGood + " Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+//   }
+// };
+
+
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(good + " Assertion Passed: " + actual + " === " + expected);
+    console.log(good + ` Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(noGood + " Assertion Failed: \"" + actual + "\" !== \"" + expected + "\"");
+    console.log(noGood + ` Assertion Failed: "${actual}" !== "${expected}"`);
   }
 };
 
